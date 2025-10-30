36529b81680bcbf1ca8fc12eaf8c080636503581
yujou <yujoupaulaschmitz@gmail.com>
2025-10-30 12:05:53 +0100

    p2

---
+++ src/h3/H3_main.java
@@ -0,0 +1,21 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 100;
+        int k = -1;
+
+    if (i > j && i<=200 && j<= 100) {
+        k = 1;
+    } else if ( i > j && i<=200 && j<= 100) {
+        k = 2;
+    }else if (i > j && i<=200 && j<= 100) {
+        k = 3;
+    } else if (i > j && i<=200 && j<= 100) {
+        k = 4;
+    }
+    System.out.println(k);
+
+    }
+}
