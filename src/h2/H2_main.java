a792cb62bf8d4a18fddaaec44b1a742532fad057
yujou <yujoupaulaschmitz@gmail.com>
2025-10-30 11:56:24 +0100

    p2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,30 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        int i =  1;
+        int j = 50;
+        int k = 500;
+        int min;
+        int max;
+
+        if (i < j && i < k) {
+            min = i;
+        } else if (j < k && j < i) {
+            min = j;
+        } else {
+            min = k;
+        }
+
+        if ( i > j && i > k) {
+            max = i;
+        } else if (j > k && j > i) {
+            max = j;
+        } else {
+            max = k;
+        }
+
+        System.out.println("Minimum: " + min);
+        System.out.println("Maximum: " + max);
+    }
+}
